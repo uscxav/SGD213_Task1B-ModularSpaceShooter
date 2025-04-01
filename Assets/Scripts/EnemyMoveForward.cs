d38116d3e130cf52eddce0680614137833fce6ab
uscxav <a_a246@student.usc.edu.au>
2025-04-01 18:09:18 +1000

    script edit
    - deleted MoveForwardConstantly.cs
    - renamed PlayerMovementScript.cs to PlayerMovement.cs
    - reformatted EnemyMoveForward.cs and PlayerMovement.cs

--- Assets/Scripts/EnemyMoveForward.cs
+++ Assets/Scripts/EnemyMoveForward.cs
@@ -4,24 +4,22 @@ using System.Collections;
 public class EnemyMoveForward : MonoBehaviour {
 
     private float acceleration = 75f;
-
     private float initialVelocity = 2f;
-
     private Rigidbody2D ourRigidbody;
 
-    // Use this for initialization
+   
     void Start()
     {
+        // apply initialVelocity to rigidbody
         ourRigidbody = GetComponent<Rigidbody2D>();
-
         ourRigidbody.velocity = Vector2.down * initialVelocity;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+        // add force using acceleration
         Vector2 ForceToAdd = Vector2.down * acceleration * Time.deltaTime;
-
         ourRigidbody.AddForce(ForceToAdd);
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+            /* SerializeField exposes this value to the Editor, but not to other Scripts!
+               It is "pseudo public"
+               HorizontalPlayerAcceleration indicates how fast we accelerate Horizontally */
+            [SerializeField]
+            private float f_horPlayAccel = 5000f;
+            private Rigidbody2D ourRigidbody;
+
+
+    void Start() 
+    {
+        // store a reference to ourRigidBody component
+        ourRigidbody = GetComponent<Rigidbody2D>(); 
+    }
+ 
+    void Update() 
+    {
+        // get HorizonalInput and applies force to it using f_horPlayAccel
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        if (HorizontalInput != 0.0f) {
+            Vector2 ForceToAdd=Vector2.right * HorizontalInput * f_horPlayAccel * Time.deltaTime;
+            ourRigidbody.AddForce(ForceToAdd);
+            
+        }
+    }
+}
